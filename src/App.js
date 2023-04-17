d0f155111965a43c3913f8fedcf7948f9f65b651
unknown <J4sumit@gmail.com>
2023-04-17 13:50:09 +0530

    class component and nested component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+// import React,{Component} from 'react';
 
 import './App.css';
 import User from './User'
 function App() {
+function Inner()
+{
+  return(
+    <h1>Inner function</h1>
+  )
+}
   return (
     <div className="App">
       <h1>Hello world  </h1>
       <User />
+      {Inner()}
     </div>
   );
 }
+
 
 export default App;
