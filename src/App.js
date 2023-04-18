3e41ca471bd795f9a3a67b5806d8165c0a8eaa4c
unknown <J4sumit@gmail.com>
2023-04-18 20:17:29 +0530

    State in class and fucntion component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-// import React,{Component} from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
-// import User from './User'
 
-function App() {
-  let data ="Sumit Chaurasiya";
-function apple(){
-  data ="Amit Chaurasiya";
-  alert(data);
-}
-  return (
-    <div className="App">
+function App(){
+  const[data, setData] =useState(0);
+  function apple()
+  {
+setData(data+1);
+
+  }
+  return(
+    <div className='App'>
       <h1>{data}</h1>
-    <button onClick = {apple}> Click me </button>
-    
+      <button onClick={apple}>update data</button>
     </div>
-  );
+  )
+
 }
-
-
 export default App;
+
+
+// class App extends React.Component
+// {
+
+//   constructor()
+//   {
+//     super()
+//     this.state= {
+//       data:0
+//     }
+//   }
+//   apple()
+//   {
+//     this.setState({data: this.state.data+1});
+//   }
+//   render(){
+//     return(
+//       <div className="App">
+//       <p> {this.state.data}</p>
+//       <button onClick={()=>this.apple()}>update data</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/User.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-function User()
-{
-return React.createElement("div", null, React.createElement("h1", null, "Heading"));
-}
-export default User;
