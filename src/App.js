ae3f0d2a1bf4eb3b134bde7defefbf4f9ccb738a
unknown <J4sumit@gmail.com>
2023-04-19 15:05:18 +0530

    ternury operator in jSX

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import React,{useState} from 'react';
-
+// import React,{useState} from 'react';
+import Profile from './Profile';
 
 import './App.css';
 
 
 function App(){
-  const[name,setName]= useState("");
-  const[tnc,setTnc]= useState(false);
-  const[interest,setInterest]= useState("");
-  function getFormData(e)
-  {
-    console.warn(name, tnc, interest)
-e.preventDefault()
-  }
    return(
-    <div className='App'> 
-  <h1>Handle form in react</h1><br/><br/>
-  <form onSubmit={getFormData}>
-    <input type="text" placeholder='Enter name' value= {name} onChange={(e)=> setName(e.target.value)}/> <br/><br/><br/>
-    
-    <select onChange={(e)=> setInterest(e.target.value)}>      
-      <option>Option</option>
-      <option>Marwal</option>
-      <option>DC</option>
-      </select><br/><br/>
-     <input type="checkbox" onChange={(e)=> setTnc(e.target.checked)} /> <span>Accept Terms and condition</span>
-    <br/><br/>
-    <button type='Submit'>Submit</button>
-    
-  </form>
-      </div>
+<div className='App'>
+  <Profile />
+</div>
   );
 };
 
