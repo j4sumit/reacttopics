014b61f7642b27f1a79c4912ddd79fd5adb1854d
unknown <J4sumit@gmail.com>
2023-04-21 15:01:25 +0530

    add key in table also use bootsrap in react

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
 import './App.css';
 import React from 'react';
+import {Table} from 'react-bootstrap';
 function App()
 {
   // const student =['anil','siddhu','sam','peter'];
   
   const student=[
-{
-  name:"sumit",
-  contact: 1111111111,
-  email: "xyz@gmail.com"
-},
-{
-  name:"Amit",
-  contact: 222222222,
-  email: "abc@gmail.com"
-},
-
-{
-  name:"raju",
-  contact: 44444444,
-  email: "raju@gmail.com"
-}
-
-  ]
+{ name:"sumit",  contact: 1111111111,  email: "xyz@gmail.com"},
+{ name:"Amit",  contact: 222222222,  email: "abc@gmail.com"},
+{  name:"raju",  contact: 111,  email: "raju@gmail.com"}
+]
 
   return(
     <div className='App'>
-      <h1>Handle array with List</h1>
-      <table border="1">
-      <tr >
-        <th> name </th>
-        <th> contact </th>
-        <th> email</th>
-        </tr>
-        {
-        student.map((data)=> 
-        <tr>
-          <td> {data.name} </td>
-          <td> {data.contact} </td>
-          <td>  {data.email}</td>
+      <h1>List with bootstrap</h1>
+      <Table striped variant='dark'>
+<tbody>
+          <tr>
+          <th>Name</th>
+          <th>Contact</th>
+          <th>Email</th>
           </tr>
-        )}
-</table>
-
+{
+  student.map((item,i)=>
+  /*<tr key={i}>
+   <td>{item.name}</td>
+  <td>{item.contact}</td>
+  <td>{item.email}</td> */
+item.contact===222222222?  
+<tr key={i}>
+  <td>{item.name}</td>
+  <td>{item.contact}</td>
+  <td>{item.email}</td>
+  </tr> : null
+  )
+}
+</tbody>
+        </Table>
 
     </div>
   )
