0d172d2b2e5d76bb1a3b272a4ad17cb464338b84
unknown <J4sumit@gmail.com>
2023-04-21 14:16:40 +0530

    uses Map method in reat to create table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import './App.css';
 import React from 'react';
-import './Style.css';
-import style from './custom.module.css';
-
-function App() 
+function App()
 {
+  // const student =['anil','siddhu','sam','peter'];
+  
+  const student=[
+{
+  name:"sumit",
+  contact: 1111111111,
+  email: "xyz@gmail.com"
+},
+{
+  name:"Amit",
+  contact: 222222222,
+  email: "abc@gmail.com"
+},
+
+{
+  name:"raju",
+  contact: 44444444,
+  email: "raju@gmail.com"
+}
+
+  ]
+
   return(
     <div className='App'>
-      <h1 className='primary'>Style in React</h1>
-      <h1 style={{color:'red', backgroundColor:'yellow'}}>Style in React</h1>
-      <h1 className={style.success}>Style in React</h1>
-      
+      <h1>Handle array with List</h1>
+      <table border="1">
+      <tr >
+        <th> name </th>
+        <th> contact </th>
+        <th> email</th>
+        </tr>
+        {
+        student.map((data)=> 
+        <tr>
+          <td> {data.name} </td>
+          <td> {data.contact} </td>
+          <td>  {data.email}</td>
+          </tr>
+        )}
+</table>
+
+
     </div>
-    );
+  )
 }
+
 export default App;
