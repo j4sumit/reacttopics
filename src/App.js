ef25b7b189d774e7e8d832596059eeaa83709068
unknown <J4sumit@gmail.com>
2023-05-10 11:07:04 +0530

    Map and Filter together applied on useState variable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { Button } from 'bootstrap';
 import React from 'react'
 import { useEffect, useState } from 'react';
 import "./App.css";
 
 const App = () => {
-    const [users, setUser] = useState([]);
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((json) => setUser(json));
-    }, []);
+
+    // const [users, setUser] = useState([]);
+    // useEffect(() => {
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+    //         .then((response) => response.json())
+    //         .then((json) => setUser(json));
+    // }, []);
 
 
     // const myData =() =>{
@@ -16,26 +18,31 @@ const App = () => {
     //     console.log(newarray);
     // }
 
-    const myData =() =>{
-        let newarray =users.filter((user)=> {
+    // const myData =() =>{
+    //     let newarray =users.filter((user)=> {
 //  return user.name === "Leanne Graham";
 // return user.name.includes("a");
-return user.id <= 5;
-        });
-setUser(newarray);
-    }
+// return user.id <= 5;
+//         });
+// setUser(newarray);
+//     }
+const [numbers, setNumbers] = useState([1,2,3,4,5,6,7,8,9]);
+const myData= ()=>{
+    let numberlessthan5 = numbers.filter((num)=> num<5).map((filterdata)=> filterdata*filterdata);
+    setNumbers(numberlessthan5); 
+}
 
     return (
         <div className='App'>
             {
-                users.map((user) =>
+                numbers.map((number) =>
                     <div>
-                        {user.name}
+                        {number}
                     </div>
                 )
 
             }
-                            <button onClick={myData}>Submit</button> 
+<button onClick={myData}>Result</button>              
         </div>
     )
 }
