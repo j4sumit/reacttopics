81f145960e76102bf5352f6085245c36f50fa72b
unknown <J4sumit@gmail.com>
2023-05-10 23:06:36 +0530

    state using class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
-import React,{Component} from "react";
+import React, { Component } from "react";
+import "./App.css";
 
-export default class App extends Component  {
-    constructor(){
-super();
-this.state= {name: "Sumit",};
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = { name: "Amit",
+    age: 28,
+arr: [1,2,3,4,5,6,7,8,9], };  
     }
-    render()
-    {
+     changeage = ()=>{
+         this.setState({age : this.state.age * 2});
+        };
 
-    return (<div>{this.state.name}</div>)
-          
-        
+    render() {
+        return(
+            <div className="App">
+            <h1>{this.state.name}</h1>
+            <h1>{this.state.age}</h1>
+            <h1>{this.state.arr.map((num)=> num *2)}</h1>
+            <button onClick={this.changeage}>New Age</button>
+            </div>
+        )
     }
-    
+
 }
