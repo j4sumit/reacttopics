560b17418434ed127d1bfed4d82ed26328cedf02
unknown <J4sumit@gmail.com>
2023-05-11 12:06:27 +0530

    event Handling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React from 'react'
 
 const App = () => {
-  // useEffect(()=>{
-  //   let Age=37;
-  //   let name="Sumit";
-  //   Age>35 ? console.log("you are too older"): 
-  //   Age===28 && name==="Sumit" ? 
-  //   console.log("your age and name has been matched") : 
-  //   console.log("your age is below 28")
-  // },[])
-  let Age=38;
-let name="Sumit";
-  return (
-    <div >
-    <h1>Inline conditional expression</h1>
-{
-    Age > 35 ? <h1>you are too older</h1>: 
-    Age===28 && name==="Sumit" ? 
-   <h1>your age and name has been matched</h1>:
-   <h1>your age is below 28</h1>
   
+ function getInput(event)
+  {
+console.log(event.target.value);
+  }
+const addNums=(a,b)=>{
+  console.log(a+b);
 }
-
-    
+  return (
+    <div>
+    <h1>Event Handling</h1>
+    <input placeholder='Type something here' onChange={getInput}></input>
+    <button onClick={()=>{addNums(10,19)}}>Sum of two num</button>
     </div>
- 
   )
 }
 
