aa9c9df5763362027a3842ca4f7b48ca95e6c12c
unknown <J4sumit@gmail.com>
2023-05-11 13:44:35 +0530

    Form in reacct

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  const details =[
-  {
-    Name: "sumit",
-    Age : 28,
-  },
- {
-   Name:"Amit",
-   Age : 30,
- },
- {
-   Name:"sanjay",
-   Age: 35
- },
- {
-   Name:"Raju",
-   Age: 24,
- }
-]
+  const[objdata, setObjdata]= useState({});
+  const getInputs =(value,name)=>{
+    // console.log({[name] : value});
+    let data= {[name] : value};
+    setObjdata({...objdata, ...data});
+  };
+
+  const submit =(event)=>{
+    event.preventDefault();
+    console.log(objdata);
+  }
+console.log(objdata);
   return (
+    <form onSubmit={submit}>
     <div>
-    <ul>
-    {details.map((data,index) => {
-return (
-<div key={index}>
-<li>{data.Name} </li>
-<li>{data.Age}</li>
-</div>
-    )})}
-    </ul>
+      
+      <input placeholder='Enter you name'
+      name='name'
+      type="text"
+       onChange={(event)=> getInputs(event.target.value, event.target.name)}
+       />
+
+      <input placeholder='Enter you Age'
+      name='age'
+      type="number"
+      onChange={(event)=> getInputs(event.target.value, event.target.name)}
+      />
+      <input placeholder='Enter you hobbies'
+      name='hobbies'
+      type="text"
+      onChange={(event)=> getInputs(event.target.value, event.target.name)}
+      />
+      <input placeholder='Enter a date'
+      name='date'
+      type="date"
+      onChange={(event)=> getInputs(event.target.value, event.target.name)}
+      />
+      <button type='submit'>Submit</button>
+      <button type='reset'>Reset</button>
     </div>
+    </form>
   )
 }
 
