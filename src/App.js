7693e9e322ecb6eefc7a7cf56dee2065c52d61ec
unknown <J4sumit@gmail.com>
2023-05-11 11:09:48 +0530

    use if useState inside useEffect to change state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const App = () => {
+const[message, setMessage] =useState("");
+  // useEffect(()=>{
+  //   let Age=37;
+  //   let name="Sumit";
+  //   Age>35 ? console.log("you are too older"): 
+  //   Age===28 && name==="Sumit" ? 
+  //   console.log("your age and name has been matched") : 
+  //   console.log("your age is below 28")
+  // },[])
+useEffect(()=>{
+  let Age=38;
+let name="Sumit";
+  Age > 35 ? setMessage("you are too older"): 
+  Age===28 && name==="Sumit" ? 
+  setMessage("your age and name has been matched") : 
+  setMessage("your age is below 28")
+},[])
 
-  useEffect(()=>{
-    let Age=37;
-    let name="Sumit";
-    Age>35 ? console.log("you are too older"): 
-    Age===28 && name==="Sumit" ? 
-    console.log("your age and name has been matched") : 
-    console.log("your age is below 28")
-  },[])
   return (
-    <div>Inline conditional expression</div>
+    <>
+    <h1>Inline conditional expression</h1>
+    <h2>message as per given data  ::   {message}</h2>
+    </>
  
   )
 }
