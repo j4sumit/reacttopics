a123e8d3d3964ae96bbb688abba0589a7c6c49eb
unknown <J4sumit@gmail.com>
2023-05-11 11:29:41 +0530

    Ternery operator without using hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 
 const App = () => {
-const[message, setMessage] =useState("");
   // useEffect(()=>{
   //   let Age=37;
   //   let name="Sumit";
@@ -10,20 +9,21 @@ const[message, setMessage] =useState("");
   //   console.log("your age and name has been matched") : 
   //   console.log("your age is below 28")
   // },[])
-useEffect(()=>{
   let Age=38;
 let name="Sumit";
-  Age > 35 ? setMessage("you are too older"): 
-  Age===28 && name==="Sumit" ? 
-  setMessage("your age and name has been matched") : 
-  setMessage("your age is below 28")
-},[])
-
   return (
-    <>
+    <div >
     <h1>Inline conditional expression</h1>
-    <h2>message as per given data  ::   {message}</h2>
-    </>
+{
+    Age > 35 ? <h1>you are too older</h1>: 
+    Age===28 && name==="Sumit" ? 
+   <h1>your age and name has been matched</h1>:
+   <h1>your age is below 28</h1>
+  
+}
+
+    
+    </div>
  
   )
 }
