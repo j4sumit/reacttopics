e16779d9db134234538131da048e73a9c1d7a267
unknown <J4sumit@gmail.com>
2023-05-29 11:49:19 +0530

    Practice on import/Export

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import "./App.css";
 import React from 'react'
-
-let styles={
-
-  Internal : {
-    color:"balck",
-  },
-  light :{
-  backgroundColor:"white",
-  color:"black"
-  },
-  dark : {
-    backgroundColor:"black",
-    color:"white"
-  }
-
-}
+// import Gallery from './Gallery.js';
+import { Profile } from './Gallery.js';
 
 function App() {
 
-  const [theme, setTheme] = React.useState(false);
-  const toggleTheme=()=> { 
-    setTheme(!theme);
-  }
-
-  return (
-    <div style={theme ? styles.lights: styles.dark}>
-      <h1 className="external">External Styling</h1>
-      <h1 >Internal Styling</h1>
-      <h1 style={
-        theme 
-        ? {color:"black", backgroundColor:"white"}
-        : {color: "white", backgroundColor:"black"}
-      }> Inline Styling</h1>
-      <button onClick={toggleTheme}>Toggle-theme </button>
-      </div>
-  )
+return (
+  <>
+<Profile/>
+{/* <Gallery /> */}
+</>
+ )
  
 }
 
